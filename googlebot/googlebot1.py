e91965ebcf6d5d579687ba65f797dfffb1ad1d44
nickrose-helmivesting <nickrose@helmivesting.com>
2017-10-14 16:17:03 -0500

    googlebot

---
+++ googlebot/googlebot1.py
@@ -0,0 +1,16 @@
+#must have pytrends installed
+from pytrends.request import TrendReq
+
+pytrends = TrendReq(hl='en-US', tz=360)
+
+print "debug 1"
+
+pytrends.build_payload(kw_list=['coinbase'], timeframe='now 1-H')
+
+print "debug 2"
+
+testdata = pytrends.interest_over_time()
+
+print "debug 3"
+
+print testdata
