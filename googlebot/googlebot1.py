e3b586ed9c232e95c3acae940828c7b74fe27e3d
Yokoko44 <james.kokonas@gmail.com>
2017-10-14 17:38:28 -0500

    Fixed Syntax
    print syntax needs parentheses

--- googlebot/googlebot1.py
+++ googlebot/googlebot1.py
@@ -3,14 +3,14 @@ from pytrends.request import TrendReq
 
 pytrends = TrendReq(hl='en-US', tz=360)
 
-print "debug 1"
+print("debug 1")
 
 pytrends.build_payload(kw_list=['coinbase'], timeframe='now 1-H')
 
-print "debug 2"
+print("debug 2")
 
 testdata = pytrends.interest_over_time()
 
-print "debug 3"
+print("debug 3")
 
-print testdata
+print(testdata)
